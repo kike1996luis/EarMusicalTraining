c6430b17485510c26027dcc4ae25197a9acdc3ce
Luis Bermúdez <44822982+kike1996luis@users.noreply.github.com>
2023-02-26 14:11:30 -0400

    Add files via upload
    -Added necessary files and built jar to run the application

---
+++ data.rs
@@ -0,0 +1,20 @@
+
+Total de ejercicios: 5
+Unidades evaluadas: 12
+Respondido correctamente: 
+    D: 1/1
+    E: 1/2
+    F: 1/1
+    Ab: 1/1
+    A: 1/2
+    Bb: 2/3
+    B: 1/2
+
+Respondido incorrectamente: 
+    D: 1/1
+    Bb: 1/3
+    B: 1/2
+
+Fecha de finalización: 11/08/2018
+Hora de finalización: 18:15:33
+FINISH
---
+++ results.rs
@@ -0,0 +1,116 @@
+﻿Fecha de comienzo: 13/06/2018
+Hora de comienzo: 22:29:14
+
+Modo Exámen: 
+
+Total de ejercicios: 5
+Unidades evaluadas: 12
+Respondido correctamente: 
+
+Respondido incorrectamente: 
+
+Fecha de finalización: 13/06/2018
+Hora de finalización: 22:30:51
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+FINISH
+Fecha de comienzo: 14/06/2018
+Hora de comienzo: 13:24:08
+
+Modo Exámen: 
+
+Total de ejercicios: 5
+Unidades evaluadas: 12
+Respondido correctamente: 
+
+Respondido incorrectamente: 
+
+Fecha de finalización: 14/06/2018
+Hora de finalización: 13:25:17
+
+
+
+
+FINISH
+Fecha de comienzo: 14/06/2018
+Hora de comienzo: 13:24:08
+
+Modo Exámen: 
+
+Total de ejercicios: 5
+Unidades evaluadas: 12
+Respondido correctamente: 
+
+Respondido incorrectamente: 
+
+Fecha de finalización: 14/06/2018
+Hora de finalización: 13:25:17
+
+
+
+
+FINISH
+Fecha de comienzo: 14/06/2018
+Hora de comienzo: 13:24:08
+
+Modo Exámen: 
+
+Total de ejercicios: 5
+Unidades evaluadas: 12
+Respondido correctamente: 
+
+Respondido incorrectamente: 
+
+Fecha de finalización: 14/06/2018
+Hora de finalización: 13:25:17
+
+
+
+
+FINISH
+Fecha de comienzo: 15/06/2018
+Hora de comienzo: 14:18:39
+
+Modo Exámen: 
+
+Total de ejercicios: 5
+Unidades evaluadas: 12
+Respondido correctamente: 
+    D: 1/2
+    G: 1/1
+    A: 1/2
+    B: 1/2
+
+Respondido incorrectamente: 
+    C: 1/0
+    C#: 1/0
+    Eb: 1/1
+    E: 1/1
+    F: 1/0
+    F#: 1/1
+    Ab: 1/0
+    Bb: 1/0
+
+Fecha de finalización: 15/06/2018
+Hora de finalización: 14:19:41
+FINISH
